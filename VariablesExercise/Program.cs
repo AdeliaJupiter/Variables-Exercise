76c7e0ba798b3f97e7a4088bcc309b84c26464e8
Adelia Johnson <132602211+AdeliaJupiter@users.noreply.github.com>
2024-03-05 11:31:52 -0500

    declared, initialized, and interpolated variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declare and initialize variables
+            string myName = "Adelia";
+            int myAge = 29;
+            char middleInitial = 'A';
+            bool isAlive = true;
+            double currentTemp = 63;
+            decimal currentPrice = 91352.95m;
+
+            //Interpolate variables in a Console.Writeline()
+            Console.WriteLine($"Name: {myName}");
+            Console.WriteLine($"Age: {myAge}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Alive: {isAlive}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
         }
     }
 }
